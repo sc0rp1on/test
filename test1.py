f505f68fbed116b3c20c87d15730625c6a7e53df
sc0rp1on <singhsooraj.baliyan@gmail.com>
2021-06-14 23:20:19 +0530

    test2

--- test1.py
+++ test1.py
@@ -1,19 +1,10 @@
 import subprocess
-from termcolor import cprint
+# from termcolor import cprint
 # from pyfiglet import figlet_format
-
 
 logo = 'Git-Commands'
 
-
-class color:
-    NOTICE = '\033[91m'
-    END = '\033[0m'
-
-
-info = color.NOTICE + '''
-Automate the process of using commands such as clone, commit, branch, pull, merge, blame and stash.\n''' + color.END
-
+info = 'Automate the process of using commands such as clone, commit, branch, pull, merge, blame and stash.\n'
 
 def run(*args):
     return subprocess.check_call(['git'] + list(args))
@@ -22,53 +13,13 @@ def commit():
     message = input("\nType in your commit message: ")
     commit_message = f'{message}'
 
-    run("commit", "-am", commit_message)
-    run("push", "-u", "origin", "master")
-
-
-def branch():
-    br = 'master'
-    br = 'main'
-    
-    choice = input("\nDo you want to push the branch right now to GitHub? (y/n): ")
-    choice = choice.lower()
-
-    if choice == "y":
-        run("push", "-u", "origin", br)
-
-    elif choice == "n":
-        print("\nOkay, goodbye!\n")
-
-    else:
-        print("\nInvalid command! Use y or n.\n")
-
-
-def pull():
-    print("\nPulls changes from the current folder if *.git is initialized.")
-
-    choice = input("\nDo you want to pull the changes from GitHub? (y/n): ")
-    choice = choice.lower()
-
-    if choice == "y":
-        run("pull")
-
-    elif choice == "n":
-        print("\nOkay, goodbye!\n")
-
-    else:
-        print("\nInvalid command! Use y or n.\n")
-
-def merge():
-    branch = input("\nType in the name of your branch: ")
-    br = f'{branch}'
-
-    run("merge", br)
+    run("add", "--all")
+    run("commit", "-m", commit_message)
+    run("push")
+    # run("push", "-u", "origin", "master")
 
 def main():
-    cprint(figlet_format(logo, font='slant'), 'green')
-    print(info + "\n")
-
-    choices = 'clone, commit, branch, pull, fetch, merge, reset, blame and stash'
+    choices = 'commit'
     print("Commands to use: " + choices)
 
     choose_command = input("Type in the command you want to use: ")
@@ -76,15 +27,6 @@ def main():
 
     if choose_command == "commit":
         commit()
-
-    elif choose_command == "branch":
-        branch()
-
-    elif choose_command == "pull":
-        pull()
-
-    elif choose_command == "merge":
-        merge()
 
     else:
         print("\nNot a valid command!")
